5baeb45ab123678ef1d56f9a7e602b1843cbf85f
abedzalloum <abdalhafeez@sitech.me>
2024-05-30 15:42:36 +0300

    some changes on jazzmin admin panel

--- config/settings.py
+++ config/settings.py
@@ -164,19 +164,19 @@ YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "Library Admin",
+    "site_title": "Admin Dashboard",
 
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "Library",
+    "site_header": "Recipe-jar Admin Dashboard",
 
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "Library",
+    "site_brand": "Recipe-jar",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
 
     # Logo to use for login form in dark themes (defaults to login_logo)
-    "login_logo_dark": None,
+    "login_logo_dark": True,
 
     # CSS classes that are applied to the logo above
     "site_logo_classes": "img-circle",
@@ -185,14 +185,7 @@ JAZZMIN_SETTINGS = {
     "site_icon": None,
 
     # Welcome text on the login screen
-    "welcome_sign": "Welcome to the library",
-
-    # Copyright on the footer
-    "copyright": "Acme Library Ltd",
-
-    # List of model admins to search from the search bar, search bar omitted if excluded
-    # If you want to use a single search field you dont need to use a list, you can use a simple string
-    "search_model": ["auth.User", "auth.Group"],
+    "welcome_sign": "Welcome to the admin panel",
 
     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
     "user_avatar": None,
